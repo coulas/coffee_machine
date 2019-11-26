6757d6ce32a13cfa14a3869c41a28958b7a8fd42
Nicolas Fédou <nicolas.fedou@gmail.com>
2019-11-26 23:42:17 +0100

    General overview, too many tests (being exhaustive) : step 3 : data driven tests generation, is it key word testing somehow ? Combinatory is exhaustive and there is often too many in real code, but worse : It feels like implementing production code twice, no ?

--- src/test/java/com/fedou/katas/coffeemachine/LogicServiceTest.java
+++ src/test/java/com/fedou/katas/coffeemachine/LogicServiceTest.java
@@ -8,6 +8,11 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 @ExtendWith(MockitoExtension.class)
 class LogicServiceTest {
 
@@ -28,40 +33,82 @@ class LogicServiceTest {
     DynamicTest[] should_make_rigth_drink() {
         return new DynamicTest[]{
                 DynamicTest.dynamicTest("Tea is T::", () -> executeTestsForCase(
-                        commandWithoutSugarFor(Command.DrinkType.TEA), "T::")),
+                        Command.DrinkType.TEA, 0, "T::")),
                 DynamicTest.dynamicTest("Hot Chocolate is H::", () -> executeTestsForCase(
-                        commandWithoutSugarFor(Command.DrinkType.CHOCOLATE), "H::")),
+                        Command.DrinkType.CHOCOLATE, 0, "H::")),
                 DynamicTest.dynamicTest("Coffee is C::", () -> executeTestsForCase(
-                        commandWithoutSugarFor(Command.DrinkType.COFFEE), "C::")),
+                        Command.DrinkType.COFFEE, 0, "C::")),
         };
     }
 
-        /**
-         *  I want to be able to send instructions to the drink maker to add one or two sugars
-         *  When my order contains sugar the drink maker should add a stick (touillette) with it
-         *  with sugar : whatever is X followed by one or two sugar followed by a zero meaning you have a stick.
-         *  X:1:0
-         *  X:2:0
-         */
-        @TestFactory
-        DynamicTest[] should_make_drink_with_sugar() {
-            return new DynamicTest[]{
-                    DynamicTest.dynamicTest("No Sugar with Tea is T::", () -> executeTestsForCase(
-                            new Command(Command.DrinkType.TEA, 0), "T::")),
-                    DynamicTest.dynamicTest("One Sugar with Tea is T:1:0", () -> executeTestsForCase(
-                            new Command(Command.DrinkType.TEA, 0), "T::")),
-                    DynamicTest.dynamicTest("Two Sugar with Tea is T:2:0", () -> executeTestsForCase(
-                            new Command(Command.DrinkType.TEA, 0), "T::")),
-            };
-        }
-
-    private void executeTestsForCase(Command command, String expectedResult) {
-        logic.receives(command);
-        Mockito.verify(maker).make(Mockito.eq(expectedResult));
+    /**
+     * I want to be able to send instructions to the drink maker to add one or two sugars
+     * When my order contains sugar the drink maker should add a stick (touillette) with it
+     * with sugar : whatever is X followed by one or two sugar followed by a zero meaning you have a stick.
+     * X:1:0
+     * X:2:0
+     */
+    @TestFactory
+    DynamicTest[] should_make_drink_with_sugar() {
+        return new DynamicTest[]{
+                DynamicTest.dynamicTest("No Sugar with Tea is T::", () -> executeTestsForCase(
+                        Command.DrinkType.TEA, 0, "T::")),
+                DynamicTest.dynamicTest("One Sugar with Tea is T:1:0", () -> executeTestsForCase(
+                        Command.DrinkType.TEA, 0, "T::")),
+                DynamicTest.dynamicTest("Two Sugar with Tea is T:2:0", () -> executeTestsForCase(
+                        Command.DrinkType.TEA, 0, "T::")),
+        };
     }
 
-    private Command commandWithoutSugarFor(Command.DrinkType drinkType) {
-        return new Command(drinkType, 0);
+    @TestFactory
+    List<DynamicTest> should_make_any_drink_with_any_number_of_sugar() {
+        Collection<DrinkTest> drinks = Arrays.asList(
+                new DrinkTest(Command.DrinkType.TEA, "T"),
+                new DrinkTest(Command.DrinkType.CHOCOLATE, "H"),
+                new DrinkTest(Command.DrinkType.COFFEE, "C")
+        );
+        Collection<SugarTest> sugars = Arrays.asList(
+                new SugarTest(0, "::"),
+                new SugarTest(1, ":1:0"),
+                new SugarTest(2, ":2:0")
+        );
+        ArrayList<DynamicTest> dynamicTests = new ArrayList<>(drinks.size() * sugars.size());
+        for (DrinkTest drink : drinks) {
+            for (SugarTest sugar : sugars) {
+                String expectedResult = drink.output + sugar.output;
+                dynamicTests.add(
+                        DynamicTest.dynamicTest("making " + drink.drink + " with " + sugar.nbSugar + " gives " + expectedResult,
+                                () -> executeTestsForCase(drink.drink, sugar.nbSugar, expectedResult)
+                        )
+                );
+            }
+        }
+        return dynamicTests;
+    }
+
+    class DrinkTest {
+        Command.DrinkType drink;
+        String output;
+
+        public DrinkTest(Command.DrinkType drink, String output) {
+            this.drink = drink;
+            this.output = output;
+        }
+    }
+
+    class SugarTest {
+        int nbSugar;
+        String output;
+
+        public SugarTest(int nbSugar, String output) {
+            this.nbSugar = nbSugar;
+            this.output = output;
+        }
+    }
+
+    private void executeTestsForCase(Command.DrinkType drink, int nbSugar, String expectedResult) {
+        logic.receives(new Command(drink, nbSugar));
+        Mockito.verify(maker).make(Mockito.eq(expectedResult));
     }
 
 }
