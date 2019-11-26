79ec6190c815c8f60f0206f95d133e4c3695490a
Nicolas Fédou <nicolas.fedou@gmail.com>
2019-11-26 23:00:20 +0100

    General overview, what I need can and shallbe described as tests : step 1 : define

--- src/test/java/com/fedou/katas/coffeemachine/LogicServiceTest.java
+++ src/test/java/com/fedou/katas/coffeemachine/LogicServiceTest.java
@@ -1,6 +1,5 @@
 package com.fedou.katas.coffeemachine;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -21,4 +20,23 @@ class LogicServiceTest {
         logic.receives(new Command(Command.DrinkType.TEA, 0));
         Mockito.verify(maker).make(Mockito.eq("T::"));
     }
+
+    // TODO : do tests for :
+    /**
+     *  The drink maker should receive the correct instructions for my coffee / tea / chocolate order
+     *  without sugar
+     *  T:: => Tea
+     *  H:: => Hot Chocolate
+     *  C:: => Coffee
+     *
+     */
+
+    /**
+     *  I want to be able to send instructions to the drink maker to add one or two sugars
+     *  When my order contains sugar the drink maker should add a stick (touillette) with it
+     *  with sugar : whatever is X followed by one or two sugar followed by a zero meaning you have a stick.
+     *  X:1:0
+     *  X:2:0
+     */
+
 }
