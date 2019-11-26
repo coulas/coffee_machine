dd1018100ba7dfef81808376ab0d4c5b8bd97c1e
Nicolas Fédou <nicolas.fedou@gmail.com>
2019-11-27 00:00:52 +0100

    Command comes from pad (say Json in a usual rest app) DrinkMaker Protocol comes from machine ( say SQL in a usual rest app) Having Command knowing protocol details is an abstraction leak (your json knows about your database) but this "one liner" is so tempting : message.append(command.drink.asLetter()) or **.toDrinkMakerProtocol())

--- src/main/java/com/fedou/katas/coffeemachine/LogicService.java
+++ src/main/java/com/fedou/katas/coffeemachine/LogicService.java
@@ -9,6 +9,10 @@ public class LogicService {
 
     public void receives(Command command) {
         StringBuilder message = new StringBuilder();
+        // Command comes from pad (say Json in a usual rest app)
+        // DrinkMaker Protocol comes from machine ( say SQL in a usual rest app)
+        // Having Command knowing protocol details is an abstraction leak (your json knows about your database)
+        // but this "one liner" is so tempting : message.append(command.drink.asLetter()) or **.toDrinkMakerProtocol())
         switch (command.drink) {
             case TEA:
                 message.append("T");
