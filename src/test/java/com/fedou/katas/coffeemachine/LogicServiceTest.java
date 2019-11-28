47ee4337b1baed9a66530a264b5b806ec6833951
Nicolas Fédou <nicolas.fedou@gmail.com>
2019-11-29 00:49:05 +0100

    simplify tests by removing useless combinatory (wait to have a bug to add those tests) and by maximizing noise/signal ratio (tests have only intentions names and significant data for the tests, implementation noise is behind method names)

--- src/test/java/com/fedou/katas/coffeemachine/LogicServiceTest.java
+++ src/test/java/com/fedou/katas/coffeemachine/LogicServiceTest.java
@@ -8,11 +8,6 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-
 @ExtendWith(MockitoExtension.class)
 class LogicServiceTest {
 
@@ -22,78 +17,35 @@ class LogicServiceTest {
     LogicService logic;
 
     @TestFactory
-    DynamicTest[] should_make_rigth_drink() {
+    DynamicTest[] should_make_right_drink() {
         return new DynamicTest[]{
-                DynamicTest.dynamicTest("Tea is T::", () -> executeTestsForCase(
-                        Command.DrinkType.TEA, 0, "T::")),
-                DynamicTest.dynamicTest("Hot Chocolate is H::", () -> executeTestsForCase(
-                        Command.DrinkType.CHOCOLATE, 0, "H::")),
-                DynamicTest.dynamicTest("Coffee is C::", () -> executeTestsForCase(
-                        Command.DrinkType.COFFEE, 0, "C::")),
+                testMakeRightDrinkWithoutSugar("Tea is T::", Command.DrinkType.TEA, "T::"),
+                testMakeRightDrinkWithoutSugar("Hot Chocolate is H::", Command.DrinkType.CHOCOLATE, "H::"),
+                testMakeRightDrinkWithoutSugar("Coffee is C::", Command.DrinkType.COFFEE, "C::"),
         };
     }
 
+    private DynamicTest testMakeRightDrinkWithoutSugar(String displayName, Command.DrinkType drink, String expectedResult) {
+        return DynamicTest.dynamicTest(displayName, () -> {
+            logic.receives(new Command(drink, 0));
+            Mockito.verify(maker).make(Mockito.eq(expectedResult));
+        });
+    }
+
     @TestFactory
-    DynamicTest[] should_make_drink_with_sugar() {
+    DynamicTest[] should_make_tea_with_sugar() {
         return new DynamicTest[]{
-                DynamicTest.dynamicTest("No Sugar with Tea is T::", () -> executeTestsForCase(
-                        Command.DrinkType.TEA, 0, "T::")),
-                DynamicTest.dynamicTest("One Sugar with Tea is T:1:0", () -> executeTestsForCase(
-                        Command.DrinkType.TEA, 1, "T:1:0")),
-                DynamicTest.dynamicTest("Two Sugar with Tea is T:2:0", () -> executeTestsForCase(
-                        Command.DrinkType.TEA, 2, "T:2:0")),
+                testMakingTeaWithSugar("No Sugar with Tea is T::", 0, "T::"),
+                testMakingTeaWithSugar("One Sugar with Tea is T:1:0", 1, "T:1:0"),
+                testMakingTeaWithSugar("Two Sugar with Tea is T:2:0", 2, "T:2:0"),
         };
     }
 
-    @TestFactory
-    List<DynamicTest> should_make_any_drink_with_any_number_of_sugar() {
-        Collection<DrinkTest> drinks = Arrays.asList(
-                new DrinkTest(Command.DrinkType.TEA, "T"),
-                new DrinkTest(Command.DrinkType.CHOCOLATE, "H"),
-                new DrinkTest(Command.DrinkType.COFFEE, "C")
-        );
-        Collection<SugarTest> sugars = Arrays.asList(
-                new SugarTest(0, "::"),
-                new SugarTest(1, ":1:0"),
-                new SugarTest(2, ":2:0")
-        );
-        ArrayList<DynamicTest> dynamicTests = new ArrayList<>(drinks.size() * sugars.size());
-        for (DrinkTest drink : drinks) {
-            for (SugarTest sugar : sugars) {
-                String expectedResult = drink.output + sugar.output;
-                dynamicTests.add(
-                        DynamicTest.dynamicTest("making " + drink.drink + " with " + sugar.nbSugar + " gives " + expectedResult,
-                                () -> executeTestsForCase(drink.drink, sugar.nbSugar, expectedResult)
-                        )
-                );
-            }
-        }
-        return dynamicTests;
-    }
-
-    class DrinkTest {
-        Command.DrinkType drink;
-        String output;
-
-        public DrinkTest(Command.DrinkType drink, String output) {
-            this.drink = drink;
-            this.output = output;
-        }
-    }
-
-    class SugarTest {
-        int nbSugar;
-        String output;
-
-        public SugarTest(int nbSugar, String output) {
-            this.nbSugar = nbSugar;
-            this.output = output;
-        }
-    }
-
-    private void executeTestsForCase(Command.DrinkType drink, int nbSugar, String expectedResult) {
-        logic.receives(new Command(drink, nbSugar));
-        Mockito.verify(maker).make(Mockito.eq(expectedResult));
+    private DynamicTest testMakingTeaWithSugar(String displayName, int nbSugar, String expectedResult) {
+        return DynamicTest.dynamicTest(displayName, () -> {
+            logic.receives(new Command(Command.DrinkType.TEA, nbSugar));
+            Mockito.verify(maker).make(Mockito.eq(expectedResult));
+        });
     }
 
 }
