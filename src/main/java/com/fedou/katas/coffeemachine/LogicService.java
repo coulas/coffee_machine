8bb371411fa8c5935e618312cf290b6d6a39c627
Nicolas Fédou <nicolas.fedou@gmail.com>
2019-11-30 10:41:52 +0100

    As I changed the design, algorithm will change in structure without changing in functionality (I have to make this true). So I inlined previous method to manipulate the whole algorithm as seen in coffee, then I manipulated the aglo as seen in others (let's say Tea).

--- src/main/java/com/fedou/katas/coffeemachine/LogicService.java
+++ src/main/java/com/fedou/katas/coffeemachine/LogicService.java
@@ -8,44 +8,56 @@ public class LogicService {
     }
 
     public void receives(Command command) {
-        StringBuilder message = new StringBuilder();
         switch (command.drink) {
             case TEA:
-                if (handleCost("Tea", 40, command.amountPaid)) return;
-                message.append("T");
-                handleSugar(command, message);
-                maker.make(message.toString());
+                if (command.amountPaid < 40) {
+                    maker.make("M:" + "Tea" + " costs " + (40 - command.amountPaid) + " cents more");
+                } else {
+                    StringBuilder message = new StringBuilder();
+                    message.append("T");
+                    if (command.nbSugar == 0) {
+                        message.append("::");
+                    } else {
+                        message.append(":")
+                                .append(command.nbSugar)
+                                .append(":0");
+                    }
+                    maker.make(message.toString());
+                }
                 break;
             case CHOCOLATE:
-                if (handleCost("Chocolate", 50, command.amountPaid)) return;
-                message.append("H");
-                handleSugar(command, message);
-                maker.make(message.toString());
+                if (command.amountPaid < 50) {
+                    maker.make("M:" + "Chocolate" + " costs " + (50 - command.amountPaid) + " cents more");
+                } else {
+                    StringBuilder message = new StringBuilder();
+                    message.append("H");
+                    if (command.nbSugar == 0) {
+                        message.append("::");
+                    } else {
+                        message.append(":")
+                                .append(command.nbSugar)
+                                .append(":0");
+                    }
+                    maker.make(message.toString());
+                }
                 break;
             case COFFEE:
-                if (handleCost("Coffee", 60, command.amountPaid)) return;
+                if (command.amountPaid < 60) {
+                    maker.make("M:" + "Coffee" + " costs " + (60 - command.amountPaid) + " cents more");
+                    return;
+                }
+                StringBuilder message = new StringBuilder();
                 message.append("C");
-                handleSugar(command, message);
+                if (command.nbSugar == 0) {
+                    message.append("::");
+                } else {
+                    message.append(":")
+                            .append(command.nbSugar)
+                            .append(":0");
+                }
                 maker.make(message.toString());
                 break;
         }
     }
 
-    private boolean handleCost(String drink, int price, int amountPaid) {
-        if (amountPaid < price) {
-            maker.make("M:" + drink + " costs " + (price - amountPaid) + " cents more");
-            return true;
-        }
-        return false;
-    }
-
-    private void handleSugar(Command command, StringBuilder message) {
-        if (command.nbSugar == 0) {
-            message.append("::");
-        } else {
-            message.append(":")
-                    .append(command.nbSugar)
-                    .append(":0");
-        }
-    }
 }
